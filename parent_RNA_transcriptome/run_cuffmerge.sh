10f20af56e9209030f99277f529f36b51726b2cf
Ruijuan Li <ruijuanli@whitney.(none)>
2017-02-15 10:46:46 -0800

    command I used to test cuffmerge

--- parent_RNA_transcriptome/run_cuffmerge.sh
+++ parent_RNA_transcriptome/run_cuffmerge.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
-cuffmerge \
+cuffmerge -p 2 \
 	-s /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa \
 	-o test \
+	-g /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus.annotation_v5.gtf \
 	--keep-tmp \
-	Ae_gtf_2.txt
+	Ae_gtf.txt
 
 echo "done"
 	 
