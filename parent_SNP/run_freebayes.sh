41347f3037cfa96b2b4b260cd8b5773b316f83bf
Ruijuan Li <ruijuanli@coloma.plb.ucdavis.edu>
2017-02-16 16:37:43 -0800

    run freebayes on whitney, still using the same version, but now with uniquely mapped reads

---
+++ parent_SNP/run_freebayes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+bamaddrg -b Ae_no_bolting_unique_mapped_rmdup.bam -s Ae -r Ae -b Ol_no_bolting_unique_mapped_rmdup.bam -s Ol -r Ol | freebayes_v0.9.21-7-g7dd41db --fasta-reference /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa --stdin --dont-left-align-indels > Ae_Ol.vcf
