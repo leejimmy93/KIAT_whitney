a1a39abc6d072a6156ba646992d9373d5490c860
Ruijuan Li <ruijuanli@coloma.plb.ucdavis.edu>
2017-02-17 09:32:27 -0800

    1st step is to sort uniquely mapped bam file

---
+++ parent_SNP/sort_bam_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+samtools sort /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/analysis/freebayes/Ae_no_bolting_unique_mapped.bam -o Ae_unqiue_sorted.bam
+
+samtools sort /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/analysis/freebayes/Ae_no_bolting_unique_mapped.bam -o Ae_unqiue_sorted.bam
