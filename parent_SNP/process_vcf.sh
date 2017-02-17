29ae842050813a6f4b814d96231b6d8008f57fa6
Ruijuan Li <ruijuanli@whitney.(none)>
2017-02-17 10:56:35 -0800

    add script to post-process vcf file from Da-Ae and Da-Ol-l

---
+++ parent_SNP/process_vcf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# example: ./process_vcf.sh file_in file_out 
+file_in=$1
+file_out=$2
+
+grep "#" $file_in > header
+grep -v "#" $file_in > content  
+
+awk 'NF==11' content > content_clean 
+cat header content_clean > $file_out
+
+rm header
+rm content
+rm content_clean
