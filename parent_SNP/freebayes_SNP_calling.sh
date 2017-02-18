da42e62bc71916e914b6890f2741e708ee2c5823
Ruijuan Li <ruijuanli@coloma.plb.ucdavis.edu>
2017-02-17 20:56:09 -0800

    something wrong with freebayes SNP calling, might because I didn't sort the bam file? so now add readgroup for my sorted bam file

--- parent_SNP/freebayes_SNP_calling.sh
+++ parent_SNP/freebayes_SNP_calling.sh
@@ -1,23 +1,23 @@
 #!/bin/bash 
 
-dir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/data/bam/no_bolting
-echo $dir
+# dir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/data/bam/no_bolting
+# echo $dir
 
 # get uniquely mapped reads
-echo "get unique" 
-samtools view $dir/Ae_no_bolting.star.trim.dir/Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > Ae_no_bolting_unique_mapped.sam
-samtools view $dir/Ol_no_bolting.star.trim.dir/Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > Ol_no_bolting_unique_mapped.sam
+# echo "get unique" 
+# samtools view $dir/Ae_no_bolting.star.trim.dir/Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > Ae_no_bolting_unique_mapped.sam
+# samtools view $dir/Ol_no_bolting.star.trim.dir/Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > Ol_no_bolting_unique_mapped.sam
 
 # transform to bam file
-echo "from sam to bam" 
-samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa Ae_no_bolting_unique_mapped.sam > Ae_no_bolting_unique_mapped.bam
+# echo "from sam to bam" 
+# samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa Ae_no_bolting_unique_mapped.sam > Ae_no_bolting_unique_mapped.bam
 
-samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa Ol_no_bolting_unique_mapped.sam > Ol_no_bolting_unique_mapped.bam
+# samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa Ol_no_bolting_unique_mapped.sam > Ol_no_bolting_unique_mapped.bam
 
 # remove PCR duplicate 
 echo "remove PCR duplicate"
-samtools rmdup -s Ae_no_bolting_unique_mapped.bam Ae_no_bolting_unique_mapped_rmdup.bam 
-samtools rmdup -s Ol_no_bolting_unique_mapped.bam Ol_no_bolting_unique_mapped_rmdup.bam
+samtools rmdup -s /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/analysis/GATK/before_vcf/no_bolting/Ae_unique_sorted.bam Ae_no_bolting_unique_mapped_rmdup.bam 
+samtools rmdup -s /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/SNP_parent/analysis/GATK/before_vcf/no_bolting/Ol_unique_sorted.bam Ol_no_bolting_unique_mapped_rmdup.bam
 
 # index bam file 
 echo "index"
