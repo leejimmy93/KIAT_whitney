4c59517c47493ec19298e6d7e5b69f331c901731
Ruijuan Li <ruijuanli@whitney.(none)>
2017-03-05 22:11:20 -0800

    script for prepare bam file and split to each chromosome

---
+++ 505/Bam_Split_By_Chrom.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+dir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/2017_winter/505/data/bioftp.org/TBD170026_20170202
+
+sample=`ls | grep "Sample" | sed 's/\///g' | sed 's/Sample_//g'`
+echo $sample
+chrom=`cat chrom_list` 
+echo $chrom
+
+for i in $sample
+        do
+        echo $i
+
+# cd to directory
+cd $dir/Sample_${i}/
+
+# echo "index"
+samtools index final.bam
+
+# change file permission
+chmod 555 final.bam
+chmod 555 final.bam.bai
+
+# split 
+	for j in $chrom
+		do 
+		echo ${j}
+		samtools view -@ 2 -b final.bam ${j} > ${j}.bam
+		samtools index ${j}.bam 
+	done
+
+cd .. 
+done 
+
---
+++ 505/Prep4Freebayes.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+dir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/2017_winter/505/data/bioftp.org/TBD170026_20170202
+
+sample=`ls | grep "Sample" | sed 's/\///g' | sed 's/Sample_//g'`
+echo $sample
+
+for i in $sample
+        do
+        echo $i
+
+# cd to directory
+cd $dir/Sample_${i}/
+# prepare for freebayes
+echo "get unique mapped reads" 
+samtools view Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > Unique.sorted.sam
+echo "from sam to bam"
+samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa Unique.sorted.sam > Unique.sorted.bam 
+echo "remove PCR dupliate"
+samtools rmdup -s Unique.sorted.bam rmdup.bam
+echo "index"
+samtools index rmdup.bam 
+echo "add readgroup"
+bamaddrg -b rmdup.bam -s $i -r $i > final.bam 
+echo "remove sam file"
+rm Unique.sorted.sam 
+cd ..
+
+done
+ 
+
+
+ 
