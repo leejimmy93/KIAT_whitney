657b2c10f7993608efe0d7ba020e72910bfd9b5e
Ruijuan Li <ruijuanli@whitney.(none)>
2017-03-14 15:04:03 -0700

    add script to compress & index vcf files

---
+++ 505/compress_index_vcf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf
+
+sample=`ls *.vcf`
+for i in $sample
+	do
+	echo $i
+	bgzip $i
+	tabix -p vcf $i.gz
+done
+
+echo "done"
+
