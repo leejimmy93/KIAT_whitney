d0e058615a6c885a2865b98cf4b444cf64831f9e
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-06 14:38:00 -0700

    script for cap3

--- parent_RNA_transcriptome/run_cap3.sh
+++ parent_RNA_transcriptome/run_cap3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
+# most ouput files are in the input file directory 
 
 cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cap3
 
-cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ae.u.fa > Ae.u.cap3.fa 
+/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/bin/CAP3/cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ae.u.fa > Ae.cap3.output  
 echo "Ae done"
 
-cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ol.u.fa > Ol.u.cap3.fa
+/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/bin/CAP3/cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ol.u.fa > Ol.cap3.output  
 echo "Ol done"
