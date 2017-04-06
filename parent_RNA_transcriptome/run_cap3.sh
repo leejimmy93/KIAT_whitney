3776dd1b300abb38156f6a939e128bb3a9d29a2a
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-06 11:37:15 -0700

    cap3 step added

---
+++ parent_RNA_transcriptome/run_cap3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cap3
+
+cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ae.u.fa > Ae.u.cap3.fa 
+echo "Ae done"
+
+cap3 /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/assembly_parent/cuffcompare/Ol.u.fa > Ol.u.cap3.fa
+echo "Ol done"
