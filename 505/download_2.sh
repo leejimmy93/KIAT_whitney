d294a9cbf039faf1f5e5027f6c7d4e2ad283e46b
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-07 13:49:40 -0700

    update

--- 505/download_2.sh
+++ 505/download_2.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 
-wget -r ftp://FNP_KSJ:FNp_71aofb@bioftp.org/TBD170026_20170202/
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/data/late_silique_batch_D
+# wget -r ftp://FNP_KSJ:FNp_71aofb@bioftp.org/TBD170026_20170202/
+wget -r ftp://FNP_KSJ:FNp_71aofb@bioftp.org/TBD170151_20170331/
+wget -r ftp://FNP_KSJ:FNp_71aofb@bioftp.org/TBD170151_20170328/Sample_505-K13/
+wget -r ftp://FNP_KSJ:FNp_71aofb@bioftp.org/TBD170151_20170328/Sample_505-K27/
